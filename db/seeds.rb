72d11595e5772c82e76484d687a953a8c1751323
Pavel Konkin <idmfani@gmail.com>
2023-05-18 23:15:37 +0300

    Remove unused fixtures and faker from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-5.times do
-  Category.create(name: Faker::Science.science)
+5.times do |i|
+  Category.create(name: "Категория #{i + 1}")
 end
