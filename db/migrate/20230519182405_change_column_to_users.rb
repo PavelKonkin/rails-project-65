d490bb8214511009ff567407e98d9f61a629fda7
Pavel Konkin <idmfani@gmail.com>
2023-05-23 16:49:20 +0300

    Make update method safe in migration for admin creation

--- db/migrate/20230519182405_change_column_to_users.rb
+++ db/migrate/20230519182405_change_column_to_users.rb
@@ -2,6 +2,6 @@
 
 class ChangeColumnToUsers < ActiveRecord::Migration[7.0]
   def change
-    User.find_by(email: 'idmfani@gmail.com').update(admin: true)
+    User.find_by(email: 'idmfani@gmail.com')&.update(admin: true)
   end
 end
